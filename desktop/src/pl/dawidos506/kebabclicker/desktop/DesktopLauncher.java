d240080f4dc52e0a33cb4479b9d17cbe0cbeb816
dawidos506 <39033795+dawidos506@users.noreply.github.com>
2018-05-06 20:52:27 +0200

    Adding files to repository

---
+++ desktop/src/pl/dawidos506/kebabclicker/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.dawidos506.kebabclicker.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.dawidos506.kebabclicker.Clicker;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Clicker(), config);
+	}
+}
